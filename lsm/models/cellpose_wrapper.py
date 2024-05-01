aaf13c34e09c0203594187e1afa6ebc3e1d30bbc
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2024-05-01 13:21:47 -0400

    added skeleton framework for model evaluation

--- lsm/models/cellpose_wrapper.py
+++ lsm/models/cellpose_wrapper.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 import torch.nn as nn
 
@@ -5,12 +7,19 @@ from cellpose import models, io
 
 
 class Cellpose(nn.Module):
-    def __init__(self, model_config: dict()):
+    def __init__(
+        self,
+        model_config: dict(),
+        device: Optional[torch.device] = torch.device("cuda:0"),
+    ):
         super(Cellpose, self).__init__()
 
         model_type = model_config["model_type"]
         self.channels = model_config["channels"]
-        self.model = models.Cellpose(model_type=model_type)
+        self.n_channels = model_config["n_channels"]
+        self.model = models.Cellpose(
+            gpu=True, nchan=self.n_channels, model_type=model_type
+        )
 
     def forward(self, x: torch.Tensor):
         masks, flows, styles, diams = self.model.eval(
@@ -24,6 +33,7 @@ def get_model(args):
     model_config = {
         "model_type": args.model.model_type,
         "channels": args.model.channels,
+        "n_channels": args.model.n_channels,
     }
 
     model = Cellpose(model_config=model_config)
@@ -32,8 +42,26 @@ def get_model(args):
 
 
 if __name__ == "__main__":
-    model = Cellpose(model_config={"model_type": "cyto3"})
+    model = Cellpose(
+        model_config={
+            "model_type": "neurips_cellpose_default",
+            "channels": None,
+            "n_channels": 3,
+        },
+        device=torch.device("cuda:0"),
+    )
     # test with 3-channel image batch
-    img = torch.randn(4, 3, 256, 256)
+    from skimage.io import imread
+
+    img_path = (
+        "/om2/user/ckapoor/lsm-data/NeurIPS22-CellSeg/Training/images/cell_00902.png"
+    )
+    img = torch.from_numpy(imread(img_path))
     out = model(img)
+    import numpy as np
+
+    print(f"uq: {np.unique(out)}")
+    import cv2 as cv
+
+    cv.imwrite("/om2/user/ckapoor/lsm-segmentation/test_00902.png", out)
     print(f"out: {out.shape}")
