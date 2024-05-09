aba33ca7116b03337e0080e4feab4afb6a04dabe
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2024-05-09 16:12:15 -0400

    benchmark anystar on a small slice of LSM data

--- lsm/models/anystar_wrapper.py
+++ lsm/models/anystar_wrapper.py
@@ -2,64 +2,64 @@ import os
 import numpy as np
 import nibabel as nib
 
-import torch
-import torch.nn as nn
+# import torch
+# import torch.nn as nn
 
 import tensorflow as tf
 from stardist.models import StarDist3D
 
 gpu_devices = tf.config.experimental.list_physical_devices("GPU")
 for device in gpu_devices:
-    tf.config.experimental.set_memory_growth(device, True)
+    tf.config.experimental.set_memory_growth(device, enable=True)
 
 
-class AnyStar(nn.Module):
-    def __init__(self, model_config: dict()):
-        super(AnyStar, self).__init__()
-
-        self.model_config = model_config
-        self.normalize = model_config["normalize"]
-        self.trainable = model_config["trainable"]
-        self.prob_thresh = model_config["prob_thresh"]
-        self.n_tiles = eval(model_config["n_tiles"])  # cast to Tuple[in5]
-        self.nms_thresh = model_config["nms_thresh"]
-        self.scale = model_config["scale"]
-
-        model_type = model_config["model_type"]
-        basedir = model_config["basedir"]
-
-        # instantiate and load model weights
-        self.model = StarDist3D(None, name=model_type, basedir=basedir)
-        self.model.load_weights(name="weights_best.h5")
-
-    def _normalize_vol(self, vol: torch.Tensor):
-        """normalize image volume intensities"""
-        vol = (vol - vol.max()) / (vol.max() - vol.min())
-        return vol
-
-    def forward(self, x: torch.Tensor):
-        self.model.trainable = self.trainable
-        self.model.keras_model.trainable = self.trainable
-
-        x = x.squeeze(0)  # unbatch data
-
-        assert x.ndim == 3, f"AnyStar only supports 3D image volumes"
-
-        x = x.detach().cpu().numpy()
-
-        # normalize intensties if needed
-        if self.normalize:
-            x = self._normalize_vol(vol=x)
-
-        masks, _ = self.model.predict_instances(
-            x,
-            prob_thresh=self.prob_thresh,
-            n_tiles=self.n_tiles,
-            nms_thresh=self.nms_thresh,
-            scale=self.scale,
-        )
-
-        return masks
+# class AnyStar(nn.Module):
+#    def __init__(self, model_config: dict()):
+#        super(AnyStar, self).__init__()
+#
+#        self.model_config = model_config
+#        self.normalize = model_config["normalize"]
+#        self.trainable = model_config["trainable"]
+#        self.prob_thresh = model_config["prob_thresh"]
+#        self.n_tiles = eval(model_config["n_tiles"])  # cast to Tuple[in5]
+#        self.nms_thresh = model_config["nms_thresh"]
+#        self.scale = model_config["scale"]
+#
+#        model_type = model_config["model_type"]
+#        basedir = model_config["basedir"]
+#
+#        # instantiate and load model weights
+#        self.model = StarDist3D(None, name=model_type, basedir=basedir)
+#        self.model.load_weights(name="weights_best.h5")
+#
+#    def _normalize_vol(self, vol: torch.Tensor):
+#        """normalize image volume intensities"""
+#        vol = (vol - vol.max()) / (vol.max() - vol.min())
+#        return vol
+#
+#    def forward(self, x: torch.Tensor):
+#        self.model.trainable = self.trainable
+#        self.model.keras_model.trainable = self.trainable
+#
+#        x = x.squeeze(0)  # unbatch data
+#
+#        assert x.ndim == 3, f"AnyStar only supports 3D image volumes"
+#
+#        x = x.detach().cpu().numpy()
+#
+#        # normalize intensties if needed
+#        if self.normalize:
+#            x = self._normalize_vol(vol=x)
+#
+#        masks, _ = self.model.predict_instances(
+#            x,
+#            prob_thresh=self.prob_thresh,
+#            n_tiles=self.n_tiles,
+#            nms_thresh=self.nms_thresh,
+#            scale=self.scale,
+#        )
+#
+#        return masks
 
 
 def get_model(args):
@@ -80,6 +80,8 @@ def get_model(args):
 
 
 def normalize_vol(vol):
+    print(f"max: {vol.max()}")
+    print(f"min: {vol.min()}")
     return (vol - vol.max()) / (vol.max() - vol.min())
 
 
@@ -87,11 +89,14 @@ def anystar_predict_dask(vol):
     model = StarDist3D(None, name="anystar-mix", basedir="model-weights")
     model.load_weights(name="weights_best.h5")
     model.trainable = False
-    model.keras_model.trainable = False
 
-    labels, _ = model.predict_instances(
-        vol,
+    labels, _ = model.predict_instances_big(
+        img=vol,
+        axes="ZYX",
+        block_size=[128, 128, 128],
         prob_thresh=0.5,
+        min_overlap=[4, 4, 4],
+        context=[4, 4, 4],
         n_tiles=(1, 1, 1),
         nms_thresh=0.3,
         scale=[1.0, 1.0, 1.0],
@@ -104,7 +109,7 @@ if __name__ == "__main__":
     model = StarDist3D(None, name="anystar-mix", basedir="model-weights")
     model.load_weights(name="weights_best.h5")
     model.trainable = False
-    # model.keras_model.trainable = False
+    model.keras_model.trainable = False
 
     fpath = "/om2/user/ckapoor/lsm-segmentation/anystar-data/NucMM-Z/img_0320_0704_0640.nii.gz"
     img = nib.load(fpath).get_fdata()
@@ -140,17 +145,79 @@ if __name__ == "__main__":
     url = "https://dandiarchive.s3.amazonaws.com/zarr/0bda7c93-58b3-4b94-9a83-453e1c370c24/"
     reader = Reader(parse_url(url))
     dask_data = list(reader())[0].data
-    scale = 0
+    scale = 3
     vol_scale = dask_data[scale][0][0]
-    # vol = vol_scale[0][0]
 
-    # normalize dask array
-    vol_norm = vol_scale.map_blocks(normalize_vol)
+    print(f"vs: {vol_scale.shape}")
+    vol_slice = vol_scale[100:200, ...].compute()
+    vol_norm = (vol_slice - vol_slice.max()) / (vol_slice.max() - vol_slice.min())
 
-    # predict labels on normalized volume
-    labels = vol_norm.map_blocks(anystar_predict_dask)
+    print(f"max: {vol_slice.max()}")
+    print(f"min: {vol_slice.min()}")
 
-    print(f"labels: {labels.shape}")
+    from tqdm import tqdm
+
+    # vol_chunk = vol_norm[1000:1100, ...]
+    labels, _ = model.predict_instances(
+        vol_norm,
+        prob_thresh=0.5,
+        n_tiles=(1, 1, 1),
+        nms_thresh=0.3,
+        scale=[1.0, 1.0, 1.0],
+        show_tile_progress=True,
+    )
+    print(f"uq: {np.unique(labels)}")
+
+    color_map = np.random.randint(0, 256, (1000, 3), dtype=np.uint8)
+    color_map[0] = [0, 0, 0]
+
+    from tifffile import imsave
+
+    for i in [50, 70]:
+        print(f"saving: {i}")
+        seg = color_map[labels[i, ...]]
+        imsave(f"gt_img_anystar_{i+100}.tiff", vol_scale[i + 100, ...])
+        imsave(f"pred_seg_anystar_{i+100}.tiff", seg[i, ...])
+
+    # for i in tqdm(range(0, array_shape[0], chunk_size[0]), desc="iterating along z"):
+    #    for j in tqdm(
+    #        range(0, array_shape[1], chunk_size[1]), desc="iterating along y"
+    #    ):
+    #        for k in tqdm(
+    #            range(0, array_shape[2], chunk_size[2]), desc="iterating along x"
+    #        ):
+
+    #            chunk = vol_norm[
+    #                i : i + chunk_size[0], j : j + chunk_size[1], k : k + chunk_size[2]
+    #            ]
+    #            # segment a chunk
+    #            labels, _ = model.predict_instances(
+    #                chunk,
+    #                prob_thresh=0.5,
+    #                n_tiles=(1, 1, 1),
+    #                nms_thresh=0.3,
+    #                scale=[1.0, 1.0, 1.0],
+    #            )
+
+    #            if len(np.unique(labels)) > 1:
+    #                viz_seg.append(labels)
+    #                viz_gt.append(chunk)
+
+    # naive loop chunking
+
+    # for i in range()
+
+    # labels, _ = model.predict_instances_big(
+    #    img=vol_scale,
+    #    axes="ZYX",
+    #    block_size=[128, 128, 128],
+    #    # prob_thresh=0.5,
+    #    min_overlap=[4, 4, 4],
+    #    context=[4, 4, 4],
+    #    # n_tiles=(1, 1, 1),
+    #    # nms_thresh=0.3,
+    #    scale=[1.0, 1.0, 1.0],
+    # )
 
     # labels, _ = model.predict_instances(
     #    vol_norm,
@@ -159,26 +226,11 @@ if __name__ == "__main__":
     #    nms_thresh=0.3,
     #    scale=[1.0, 1.0, 1.0],
     # )
-    from tifffile import imsave
-    from dask.diagnostics import ProgressBar
-
-    for i in [1000, 1200]:
-        print(f"evaluating slice 1000...")
-        print(f"saving: {i}")
-        with ProgressBar():
-            label_sl = labels[i, ...].compute()
-        print(f"lsl: {label_sl.shape}")
-        seg = cmap[label_sl[i, ...]]
-        imsave(f"anystar_seg_slice{i}.tiff", seg)
-        imsave(f"anystar_gt_slice{i}.tiff", vol_scale[i, ...])
-
     # for i in [35, 45]:
     #    cv.imwrite(f"{i}_gt.png", img[..., i])
     #    seg = cmap[labels[..., i]]
     #    cv.imwrite(f"{i}_mask.png", seg)
 
-    # color_map = np.random.randint(0, 256, (1000, 3), dtype=np.uint8)
-    # color_map[0] = [0, 0, 0]
     # seg_mask = color_map[labels]
     # cv.imwrite(f"anystar_mask.png", labels)
     ## cv.imwrite(f"anystar_mask.png", seg_mask)
